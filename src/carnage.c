cbb64cba460444e37653e4f87f2bcca0fca10ed7
HackerLacker <59232119+HackerLacker@users.noreply.github.com>
2023-02-01 17:03:54 +0300

    vracc

--- src/carnage.c
+++ src/carnage.c
@@ -6,7 +6,7 @@ int main(void)
     const unsigned char carnage_size = 7;
     for (unsigned char i = 0; i < carnage_size; i++)
     {
-        for (unsigned char j = 0; j < carnage_size-1; j++)
+        for (unsigned char j = 0; j < carnage_size; j++)
         {
             printf("%c \t", carnage[i+j]);
         }
